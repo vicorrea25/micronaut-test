95c53043dcf39380abd63add9c18ef7992171296
Chagas <victor.vcorrea@mercadolivre.com>
2023-01-19 09:43:54 -0300

    alteracoesdocker

--- src/main/java/com/example/api/ApiSingleton.java
+++ src/main/java/com/example/api/ApiSingleton.java
@@ -6,6 +6,8 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
